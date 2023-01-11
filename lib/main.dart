422dff9db110ab127dd1a3132fc720f91ef052af
GillMarMad <gillmarmad@gmail.com>
2023-01-11 13:17:40 -0600

    Title added

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Marpo APP'),
     );
   }
 }
